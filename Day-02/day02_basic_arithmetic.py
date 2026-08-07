431b99dfde4a30565b9c47cb7578ce44f15bd206
abhisheknegi19288-eng <abhisheknegi19288@gmail.com>
2026-08-07 12:17:04 +0530

    Add Day 2: Basic arithmetic operations program

---
+++ Day-02/day02_basic_arithmetic.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+sum_result = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+print("The sum is", sum_result)
+print("The subtraction is", sub)
+print("The multiplication is", mul)
