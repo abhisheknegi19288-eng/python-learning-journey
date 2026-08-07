d06418e6ee04a5ba372bbb67142cc2307d228669
abhisheknegi19288-eng <abhisheknegi19288@gmail.com>
2026-08-07 12:19:44 +0530

    Update Day 3 marks average with full student report program

--- Day-03/day03_marks_average.py
+++ Day-03/day03_marks_average.py
@@ -1,4 +1,13 @@
-total = marks_phy + marks_chem + marks_math
-average = total / 3
-print("Total Marks:", total)
-print("Average Marks:", average)
+student_name = input("Student Name: ")
+marks_phy = int(input("Enter Physics marks: "))
+marks_math = int(input("Enter Maths marks: "))
+marks_chemi = int(input("Enter Chemistry marks: "))
+marks_total = marks_phy + marks_math + marks_chemi
+marks_avg = marks_total/3
+print("------ Student Report ------")
+print("Student Name: ", student_name)
+print("Physics: ", marks_phy)
+print("chemistry:", marks_chemi)
+print("maths:", marks_math)
+print("Total:", marks_total)
+print("Average: ", marks_avg)
