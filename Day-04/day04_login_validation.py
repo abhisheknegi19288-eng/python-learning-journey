a41a41d4ca0e76fd5f5072ac499177bba27b7e0b
abhisheknegi19288-eng <abhisheknegi19288@gmail.com>
2026-08-07 12:23:01 +0530

    Add Day 4: Simple login validation

---
+++ Day-04/day04_login_validation.py
@@ -0,0 +1,6 @@
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+if username == "admin" and password == "python123":
+    print("Login Successful")
+else:
+    print("invalid username or password")
